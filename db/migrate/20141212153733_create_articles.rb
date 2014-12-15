ab1332d6e753ba97a0080abe57ffaaf3eb86bc71
stacybanks514 <stacybanks514@gmail.com>
2014-12-15 16:29:01 -0500

    fixing migration issue

--- db/migrate/20141212153733_create_articles.rb
+++ db/migrate/20141212153733_create_articles.rb
@@ -1,10 +1,8 @@
 class CreateArticles < ActiveRecord::Migration
   def change
-    drop_table :articles
     create_table :articles do |t|
       t.string :title
       t.text :text
-
       t.timestamps
     end
   end
