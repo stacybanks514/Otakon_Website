e6693b137b0aca4f3fb40cf23b6b4c014517be2d
stacybanks514 <stacybanks514@gmail.com>
2014-12-16 23:48:50 -0500

    wrote some test

--- spec/features/controlflow_spec.rb
+++ spec/features/controlflow_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Managing Control Flow" do
+describe "Root Page" do
   # def click_test(x)
   #   find_link('x').visible?
   #   find_link('x').click
@@ -72,6 +72,58 @@ describe "Managing Control Flow" do
 
 end
 
+describe "About Page" do
+
+  it "user should see About page" do
+    visit '/welcome/about'
+    expect(page).to have_content 'About'
+    expect(page).to have_content 'Mission Statement'
+    expect(page).to have_content 'Private Policy'
+    expect(page).to have_content 'Otakon Mascots'
+  end
+
+  it "user should see mission statement" do
+    visit '/welcome/about'
+    expect(page).to have_css('p', text: 'fans')
+  end
+
+  # it "user should see mascot images" do
+  #   visit '/welcome/about'
+  #   expect(page).to have_content('map_hiroko.gif')
+  # end
+
+end
+
+describe "Community Page" do
+
+  it "user should see About page" do
+    visit '/welcome/community'
+    expect(page).to have_content 'Clubs'
+    expect(page).to have_content 'Community: Otakon Cosplay Coordination'
+  end
+
+end
+
+describe "Contact Page" do
+
+  it "user should see About page" do
+    visit '/welcome/about'
+    expect(page).to have_content 'Contact'
+  end
+
+end
+
+describe "Convention Page" do
+
+  it "user should see convention page headlines" do
+    visit '/welcome/about'
+    # expect(page).to have_css('h1', text: 'Convention Information')
+    expect(page).to have_content 'Tickets'
+    expect(page).to have_content 'Hours of Operation'
+    page.has_selector?('table tr')
+  end
+
+end
 
 describe "Directions Page" do
 
@@ -85,19 +137,35 @@ describe "Directions Page" do
     expect(page).to have_css('iframe')
   end
 
-end
-
-
-describe "About Page" do
-
-  it "user should see About page" do
-    visit '/welcome/about'
-    expect(page).to have_content 'About'
+  it "user should see all roads to Otakon" do
+    visit '/welcome/directions'
+    # expect(page).to have_css(p, text: 'FAQ')
+    expect(page).to have_content 'All Road lead to Otakon!'
+    expect(page).to have_content 'This FAQ is aimed'
   end
 
-    it "user should see mission statement" do
-    visit '/welcome/about'
-    expect(page).to have_css('p', text: 'fans')
+end
+
+describe "Join Page" do
+
+  it "user should see Join our team" do
+    visit '/welcome/join'
+    expect(page).to have_content 'Volunteer'
+  end
+
+end
+
+describe "Admin page" do
+
+  it "Admin should be abel to log in" do
+    visit '/admins/sign_in'
+    expect(page).to have_css('h2', text: 'Administration Log in')
+    within ('#field1') do
+      fill_in 'email', :with => 'stacy@stacy.com'
+    end
+    within ('#field2') do
+      fill_in 'password', :with => '1234567890'
+    end
   end
 
 end
